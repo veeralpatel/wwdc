9381d962630bb38cb1d0479f97421f4008756de6
Veeral Patel <veeeralp@me.com>
2013-04-24 22:35:38 -0400

    Initial Commit

---
+++ AboutMe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AboutMe
+//
+//  Created by Veeral Patel on 4/24/13.
+//  Copyright (c) 2013 Veeral Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
